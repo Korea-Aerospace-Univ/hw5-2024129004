2ab1dd2108ce8590e004a1535872ec341d2c1565
2024129004 <0325won@gmail.com>
2026-04-29 15:54:43 +0900

    Update main.c

---
+++ p1/main.c
@@ -0,0 +1,35 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int N, A=0, B=0, C=0, change;
+    bool buy = false;
+    
+    scanf("%d", &N);
+    change=N;
+    
+    for (int i=1; i<=11; i++) {
+        A++;
+        for (int j=1; j<=6; j++) {
+            B+=2;
+            for (int k=1; k<=50; k++) {
+                C++;
+                
+                change = N - (900*A + 750*B + 200*C);
+                
+                if (change==0 && (C<A||C<B)) {
+                    buy = true;
+                    printf("%d %d %d\n", A, B, C);
+                }
+                
+            }
+            
+        }
+        
+    }
+    
+    if (!buy) printf("None\n");
+
+    return 0;
+}
