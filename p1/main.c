99287252187eafd822d390739dd10a3b93622a30
2024129004 <0325won@gmail.com>
2026-04-29 16:30:00 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -8,9 +8,9 @@ int main(void)
     
     scanf("%d", &N);
     
-    for (int i=1; N>(900*i); i++) {
-        for (int j=2; N>(750*j); j+=2) {
-            for (int k=1; N>(200*k); k++) {
+    for (int i=1; N>=(900*i); i++) {
+        for (int j=2; N>=(750*j); j+=2) {
+            for (int k=1; N>=(200*k); k++) {
                 
                 if ((N==(900*i+750*j+200*k)) && (k<i||k<j)) {
                     buy = true;
