6b0afec96df02ca890a29c9c7f32b20ef72c34e9
2024129004 <0325won@gmail.com>
2026-04-29 16:15:53 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -9,7 +9,7 @@ int main(void)
     change=N;
     
     for (int i=1; N/(900*i)>0; i++) {
-        for (int j=1; N/(750*j)>0; j++) {
+        for (int j=2; N/(750*j)>0; j+=2) {
             for (int k=1; N/(200*k)>0; k++) {
                 
                 change = N - (900*i + 750*j + 200*k);
